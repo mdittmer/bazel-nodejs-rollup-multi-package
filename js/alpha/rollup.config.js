0473d04ea25e6d8193297f4a7db037680c161f3d
Mark Dittmer <markdittmer@google.com>
2022-04-23 12:39:10 -0400

    local_repository() attempt not working
    Works:
    
    ```
    $ cd js/alpha
    $ bazel build //:bundle.cjs
    ```
    
    Does not work:
    
    ```
    $ bazel build @alpha//:bundle.cjs
    ERROR: Skipping '@alpha//:bundle.cjs': error loading package '@alpha//': Unable to find package for @npm//@bazel/rollup:index.bzl: The repository '@npm' could not be resolved: Repository '@npm' is not defined.
    WARNING: Target pattern parsing failed.
    ERROR: error loading package '@alpha//': Unable to find package for @npm//@bazel/rollup:index.bzl: The repository '@npm' could not be resolved: Repository '@npm' is not defined.
    INFO: Elapsed time: 0.097s
    INFO: 0 processes.
    FAILED: Build did NOT complete successfully (0 packages loaded)
        currently loading: @alpha//
    ```

--- js/alpha/rollup.config.js
+++ js/alpha/rollup.config.js
@@ -6,7 +6,6 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'index.js',
   output: {
-    file: 'out/alpha.cjs.js',
     format: 'cjs',
   },
   plugins: [nodeResolve(), commonjs({ignoreDynamicRequires: true})]
